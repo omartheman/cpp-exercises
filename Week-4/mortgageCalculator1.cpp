78ccf59df2d298b7af1a11641ba1b49a11a2cdc8
Omar Shishani <omarshishanischool@gmail.com>
2024-09-18 16:59:06 -0700

    Format mortgage output correctly

--- Week-4/mortgageCalculator1.cpp
+++ Week-4/mortgageCalculator1.cpp
@@ -16,26 +16,45 @@ int main()
   int p;
   double r; //monthly decmial interest rate 
   double annualInterestRate; 
-  double monthlyPayment; 
   int n; //Number of monthly payments
+  int yearsForPayback;
 
-  p = 5000; //Mortgage amount
-  annualInterestRate = 0.07;
-  n = 36;
+  p = 250000; //Mortgage amount
+  annualInterestRate = 0.10567;
+  yearsForPayback = 30; 
+  n = yearsForPayback * 12; //Months for payback
   r =  annualInterestRate / 12; //Monthly decimal interest rate
 
   double interestRateFactor;
   interestRateFactor = pow((1 + r), n);
 
+  double monthlyPayment; 
   monthlyPayment = p * interestRateFactor * r / (interestRateFactor - 1);
 
-  cout << "Monthly payment (calculated output) = $" << monthlyPayment << endl; 
+  double annualInterestRateDisplay; 
+  annualInterestRateDisplay = annualInterestRate * 100; 
+
+  cout << "Amount borrowed (programmer input) = $" << p << endl; 
+  cout << "Annual interest rate (programmer input) = "; 
+  
+  cout.setf(ios::fixed|ios::showpoint);
+  cout << setprecision(3); 
+  cout << annualInterestRateDisplay << "%" << endl;
+
+  cout << "Payback period (programmer input) = ";
+  cout << setprecision(0); 
+  cout << yearsForPayback << " Years" << endl; 
+
+  cout << "Monthly payment (calculated output) = $";
   //Format output 
   cout.setf(ios::fixed|ios::showpoint);
   cout << setprecision(2); 
+  cout << monthlyPayment << endl;
+
+  cout << endl; // Extra blank line
   // identifying output statements
   cout << "Programmer: Omar Shishani" << endl;
-  cout << "Description: This program converts pounds" << endl;
-  cout << " to kilograms. The input is from the console keyboard," << endl;
-  cout << " and output is to the console screen." << endl;
+  cout << "Description: This program calculates the monthly" << endl;
+  cout << "payment on a loan. The input comes from the programmer," << endl;
+  cout << "and output is to the console screen." << endl;
 }
