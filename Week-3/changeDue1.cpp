3979efa04fa8bcbcc322fa6aa9ce0bbec8815674
Omar Shishani <omarshishanischool@gmail.com>
2024-09-19 19:19:10 -0700

    Update bills down to one hundreds

--- Week-3/changeDue1.cpp
+++ Week-3/changeDue1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-  int cashPayment = 45000;
+  int cashPayment = 45672;
   int amountTendered = 100000;
   int changeDue = amountTendered - cashPayment;
 
@@ -18,6 +18,14 @@ int main()
   changeDue = changeDue % 100000;
   int tenThousands = changeDue / 10000;
   changeDue = changeDue % 10000;
+  int fiveThousands = changeDue / 5000; 
+  changeDue = changeDue % 5000;
+  int oneThousands = changeDue / 1000; 
+  changeDue = changeDue % 1000; //Remainder after paying out all 5000 bills
+  int fiveHundreds = changeDue / 500; 
+  changeDue = changeDue % 500; 
+  int oneHundreds = changeDue / 100; 
+  changeDue = changeDue % 100; 
 
   int twos;
   int ones; 
@@ -25,6 +33,12 @@ int main()
   twos = 2; 
   ones = 1; 
 
+  cout << " this many hundred thousand dollar bills: " << hundredThousands << endl;
+  cout << " this many ten thousand dollar bills: " << tenThousands << endl;
+  cout << " this many five thousand dollar bills: " << fiveThousands << endl;
+  cout << " this many one thousand dollar bills: " << oneThousands << endl;
+  cout << " this many five hundred dollar bills: " << fiveHundreds << endl;
+  cout << " this many one hundred dollar bills: " << oneHundreds << endl;
   cout << " this many two dollar bills: " << twos << endl;
   cout << " this many one dollar bills: " << ones << endl;
 }
