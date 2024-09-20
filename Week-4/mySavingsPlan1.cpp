0058ea9ff47c7849ea7088b7a7ccf4a050b534ef
Omar Shishani <omarshishanischool@gmail.com>
2024-09-19 19:07:58 -0700

    Add file for savings plan week 4

---
+++ Week-4/mySavingsPlan1.cpp
@@ -0,0 +1,20 @@
+// Lab 4.3
+// Programmer: Omar Shishani
+// Editor(s) used: VSCode
+// Compiler(s) used: Apple clang version 14.0.0
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+int main()
+{
+  // identifying output statements
+  cout << endl; // Extra line 
+  cout << "Programmer: Omar Shishani" << endl;
+  cout << "Description: This program converts pounds" << endl;
+  cout << " to kilograms. The input is from the console keyboard," << endl;
+  cout << " and output is to the console screen." << endl;
+}
