fd96e93cccdc8e114ea70eb5417e5f7d81992440
Omar Shishani <omarshishanischool@gmail.com>
2024-09-20 11:00:53 -0700

    Add notes to myDays; Start savings plan exercise
    Copy savings plan example from book.

--- Week-3/myDays.cpp
+++ Week-3/myDays.cpp
@@ -13,7 +13,7 @@ int main()
   dueDate = "Sep 29, 2024"; 
 
   int leapYearsSinceBirthday;
-  leapYearsSinceBirthday = 7; 
+  leapYearsSinceBirthday = 7; //1994 was not a leap year. 2024 is a leap year, but the last day of the year is excluded from the end of the year, so this one is not counted.  
 
   int daysInLeapYear = 364; 
 
@@ -26,16 +26,21 @@ int main()
   int numberDaysInSeptember = 30; 
   int daysOfFirstSeptemberIWasAlive;
   int daysInFirstSepBeforeMyBirthday;
+
   daysInFirstSepBeforeMyBirthday = 3; 
   daysOfFirstSeptemberIWasAlive = numberDaysInSeptember - daysInFirstSepBeforeMyBirthday; 
+
   cout << "daysOfFirstSeptemberIWasAlive: " << daysOfFirstSeptemberIWasAlive << endl;
 
   int numberDaysInOctober = 31;  
   int numberDaysInNovember = 30;  
   int numberDaysInDecember = 31; 
 
+  //Number of days I was alive in the first year of life 
   int daysInFirstYearIWasAlive = daysOfFirstSeptemberIWasAlive + numberDaysInOctober + numberDaysInNovember + numberDaysInDecember;
   cout << "daysInFirstYearIWasAlive: " << daysInFirstYearIWasAlive << endl;
+
+  
 
   cout << "DOB: " << myDOB << endl;
   cout << "Due: " << dueDate << endl; 
--- Week-4/mySavingsPlan1.cpp
+++ Week-4/mySavingsPlan1.cpp
@@ -5,12 +5,34 @@
 //
 
 #include <iostream>
+#include <iomanip>
 #include <string>
 
 using namespace std; 
 
+#include <cmath> 
+
 int main()
 {
+
+  //input values 
+  int years = 10;
+  int D = 100; 
+
+  // output (calculated) values
+  double p = 0.075 / 12; //7.5% annual interest rate 
+  double T = years * 12; 
+  double S = D * ((pow(1 + p, T) - 1) / p);
+
+  // echoing input values, unformatted 
+  cout << "In " << years << " years, $"; 
+  cout << D << " deposited per month will grow to $"; 
+
+  // formatting output (see 4.2) 
+  cout.setf(ios::fixed|ios::showpoint);
+  cout << setprecision(2); 
+  cout << S << "." << endl; 
+
   // identifying output statements
   cout << endl; // Extra line 
   cout << "Programmer: Omar Shishani" << endl;
