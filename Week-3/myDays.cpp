9463e61e69fef443febbc78365c7f22de6103f7a
Omar Shishani <omarshishanischool@gmail.com>
2024-09-19 19:47:11 -0700

    Calculate daysInFirstYearIWasAlive

--- Week-3/myDays.cpp
+++ Week-3/myDays.cpp
@@ -12,6 +12,31 @@ int main()
   string dueDate;
   dueDate = "Sep 29, 2024"; 
 
+  int leapYearsSinceBirthday;
+  leapYearsSinceBirthday = 7; 
+
+  int daysInLeapYear = 364; 
+
+  int daysInNormalYear = 365; 
+
+  int yearsSinceBirthday = 2024 - 1994; 
+  cout << "yearsSinceBirthday: " << yearsSinceBirthday << endl;
+
+  int numberDaysInFirstYear;
+  int numberDaysInSeptember = 30; 
+  int daysOfFirstSeptemberIWasAlive;
+  int daysInFirstSepBeforeMyBirthday;
+  daysInFirstSepBeforeMyBirthday = 3; 
+  daysOfFirstSeptemberIWasAlive = numberDaysInSeptember - daysInFirstSepBeforeMyBirthday; 
+  cout << "daysOfFirstSeptemberIWasAlive: " << daysOfFirstSeptemberIWasAlive << endl;
+
+  int numberDaysInOctober = 31;  
+  int numberDaysInNovember = 30;  
+  int numberDaysInDecember = 31; 
+
+  int daysInFirstYearIWasAlive = daysOfFirstSeptemberIWasAlive + numberDaysInOctober + numberDaysInNovember + numberDaysInDecember;
+  cout << "daysInFirstYearIWasAlive: " << daysInFirstYearIWasAlive << endl;
+
   cout << "DOB: " << myDOB << endl;
   cout << "Due: " << dueDate << endl; 
 }
