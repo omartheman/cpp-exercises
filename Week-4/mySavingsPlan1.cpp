50c9c989c02002e567645b0f53401587e77a21df
Omar Shishani <omarshishanischool@gmail.com>
2024-09-20 11:11:44 -0700

    Finish calculation and output notation
    Finish calc for savings plan.

--- Week-4/mySavingsPlan1.cpp
+++ Week-4/mySavingsPlan1.cpp
@@ -16,27 +16,34 @@ int main()
 {
 
   //input values 
-  int years = 10;
-  int D = 100; 
+  int ageIRetire = 50; 
+  int currentAge = 30; 
+  int yearsUntilIRetire = ageIRetire - currentAge;
+  int D = 1500; //Amount deposited per month
 
   // output (calculated) values
-  double p = 0.075 / 12; //7.5% annual interest rate 
-  double T = years * 12; 
+  double annualInterestRatePercentage = 10.00;
+  double annualInterestRate = annualInterestRatePercentage / 100; //annual interest rate 
+  double p = annualInterestRate / 12; 
+  double T = yearsUntilIRetire * 12; 
   double S = D * ((pow(1 + p, T) - 1) / p);
 
   // echoing input values, unformatted 
-  cout << "In " << years << " years, $"; 
-  cout << D << " deposited per month will grow to $"; 
-
-  // formatting output (see 4.2) 
+  cout << "In " << yearsUntilIRetire << " years, ";
   cout.setf(ios::fixed|ios::showpoint);
   cout << setprecision(2); 
+  cout << "and at " << annualInterestRatePercentage << "% interest, "; 
+  cout << "$" << D << " deposited per month will grow to $"; 
+
+  // formatting output (see 4.2) 
   cout << S << "." << endl; 
 
   // identifying output statements
-  cout << endl; // Extra line 
+  cout << endl; // Extra line
   cout << "Programmer: Omar Shishani" << endl;
-  cout << "Description: This program converts pounds" << endl;
-  cout << " to kilograms. The input is from the console keyboard," << endl;
+  cout << "Description: This program calculates the amount" << endl;
+  cout << " of money that will be saved after a number of years";
+  cout << " from a given monthly deposit and interest rate.";
+  cout << " The inputs are from the programmer," << endl;
   cout << " and output is to the console screen." << endl;
 }
