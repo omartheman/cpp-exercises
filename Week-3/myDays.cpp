253f81118083320578eb8df7e0d380b8220eb72d
Omar Shishani <omarshishanischool@gmail.com>
2024-09-19 19:37:25 -0700

    Start myDays function

---
+++ Week-3/myDays.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std; 
+
+int main()
+{
+  string myDOB;
+  myDOB = "Sep 4, 1994";
+
+  string dueDate;
+  dueDate = "Sep 29, 2024"; 
+
+  cout << "DOB: " << myDOB << endl;
+  cout << "Due: " << dueDate << endl; 
+}
