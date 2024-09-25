44cd2408f2b885faa49f1667a653e7d2f0d58617
Omar Shishani <omarshishanischool@gmail.com>
2024-09-24 17:23:54 -0700

    Calculate number of days correctly

--- Week-3/myDays.cpp
+++ Week-3/myDays.cpp
@@ -10,7 +10,9 @@ int main()
   myDOB = "Sep 4, 1994";
 
   string dueDate;
-  dueDate = "Sep 29, 2024"; 
+  dueDate = "Sep 29, 2024";
+
+  int days = 0;
 
   int leapYearsSinceBirthday;
   leapYearsSinceBirthday = 7; //1994 was not a leap year. 2024 is a leap year, but the last day of the year is excluded from the end of the year, so this one is not counted.  
@@ -40,12 +42,33 @@ int main()
   int daysInFirstYearIWasAlive = daysOfFirstSeptemberIWasAlive + numberDaysInOctober + numberDaysInNovember + numberDaysInDecember;
   cout << "daysInFirstYearIWasAlive: " << daysInFirstYearIWasAlive << endl;
 
+  //Add to "days" the result of this multiplication: 365 times the number of years in which you've lived.
+  days = days + 365 * 30; 
 
-  int daysInMiddleYears = 365 * (2023 - 1995) - leapYearsSinceBirthday;
+  //Add to "days" the number of those years that were leap years.
+  days = days + leapYearsSinceBirthday; 
+
+  //Subtract from "days" the number of days in your birth year before you were born.
+  days = days - (365 - daysInFirstYearIWasAlive); 
+  cout << "days in first year before i was alive " << endl << days << endl << endl;
+
+  int daysLeftInSep = 30 - 29; 
+
+  int daysAfterDueDate = (daysLeftInSep + 31 + 30 + 31); //Days left in sep, oct, nov, dec
+
+  //Subtract from "days" the number of days remaining in the current year after the due date.
+  days = days - daysAfterDueDate; 
+
+  cout << "days in first year before i was alive " << endl << days << endl << endl;
+
+  cout << "DOB: " << myDOB << endl; 
+  cout << "Due: " << dueDate << endl; 
+  //Output the value of "days" with an appropriate label.
+  cout << "Days: " << days << endl; 
+
+  int daysInMiddleYears = 365 * (2023 - 1995) + leapYearsSinceBirthday;
   cout << "daysInMiddleYears since i was alive (1995 - 2023), including leap years: " << endl << daysInMiddleYears << endl << endl;
 
   int daysIn2024; //Days in the year 2024 I have been alive 
 
-
-  cout << "Due: " << dueDate << endl; 
 }
