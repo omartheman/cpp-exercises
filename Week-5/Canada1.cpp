c4be6b3fb533761a2044eb7fedd4d1f2f0129716
Omar Shishani <omarshishanischool@gmail.com>
2024-09-25 17:36:54 -0700

    Create Canada1.cpp

---
+++ Week-5/Canada1.cpp
@@ -0,0 +1,21 @@
+// Lab 5.4
+// Programmer: Omar Shishani
+// Editor(s) used: VSCode
+// Compiler(s) used: Apple clang version 14.0.0
+//
+
+#include <iostream>
+
+using namespace std; 
+
+int main()
+{
+
+  // identifying output statements
+  cout << endl; 
+  cout << "Programmer: Omar Shishani" << endl;
+  cout << "Description: This program calculates the" << endl;
+  cout << " distance a lighthouse can be seen based on its height." << endl;
+  cout << " The input is from the user," << endl;
+  cout << " and output is to the console screen." << endl;
+}
