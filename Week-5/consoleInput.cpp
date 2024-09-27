90ddcfd5fc1743196ff97f3601152538898a9656
Omar Shishani <omarshishanischool@gmail.com>
2024-09-26 20:46:14 -0700

    Finish input program

--- Week-5/consoleInput.cpp
+++ Week-5/consoleInput.cpp
@@ -15,21 +15,31 @@ int main()
   int age; 
   cout << "Please enter your age: ";
   cin >> age; 
+  cin.ignore(1000, 10); 
 
-  cout << "Please enter your name: ";
   string name;
-  cin >> name; 
+  cout << "Please enter your name: ";
+  getline(cin, name); 
 
   double temp;
   cout << "Please enter the current outside temperature: ";
   cin >> temp; 
+  cin.ignore(1000, 10); 
 
-  cout << name << " is " << age << " years old. It's " << temp << " degrees Fahrenheit in Pleasant Hill.";  
+  string city; 
+  cout << "Please enter the name of the city you live in: ";
+  getline(cin, city); 
+
+  cout << endl; 
+  cout << name << " is " << age << " years old." << endl;
+  cout << "It's " << temp << " degrees Fahrenheit in " << city << "." << endl; 
 
   // identifying output statements
   cout << endl; // Extra line 
   cout << "Programmer: Omar Shishani" << endl;
-  cout << "Description: This program converts pounds" << endl;
-  cout << " to kilograms. The input is from the console keyboard," << endl;
+  cout << "Description: This program takes 4 inputs--" << endl;
+  cout << "age, name, temperature, and city--" << endl;
+  cout << " and writes them to the console in a sentence." << endl; 
+  cout << " The inputs are from the user," << endl;
   cout << " and output is to the console screen." << endl;
 }
