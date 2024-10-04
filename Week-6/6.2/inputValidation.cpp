575b3c4245bac4f16dc1f79d4275369eace43803
Omar Shishani <omarshishanischool@gmail.com>
2024-10-03 17:24:19 -0700

    Take user inputs for interest

--- Week-6/6.2/inputValidation.cpp
+++ Week-6/6.2/inputValidation.cpp
@@ -3,6 +3,7 @@
 // Editor(s) used: VSCode
 // Compiler(s) used: Apple clang version 14.0.0
 //
+
 #include <iomanip>
 #include <iostream>
 #include <string>
@@ -11,12 +12,39 @@ using namespace std;
 
 int main()
 {
+
+  while (true) {
+    cout << "Please enter the password: "; 
+    string password; 
+    cin >> password;
+    cin.ignore(1000, 10); 
+    if (password == "password-1"){
+      break; 
+    }
+  }
+
   // input values 
-  int years = 10;
-  int D = 100; 
+  int years;
+
+  cout << "Please enter the number of years until you withdraw your investment: ";
+  cin >> years; 
+  cout << endl; 
+
+  int D; //Amount deposited per month 
+
+  cout << "Please enter the amount in dollars that you will deposit per month: "; 
+  cin >> D; 
+  cout << endl; 
+
+  cout << "Please enter the annual interest rate, with any amount of decimals: ";
+
+  double interestPercentage;
+  cin >> interestPercentage;
+
+  double interestDecimal = interestPercentage / 100; 
 
   // output (calculated) values
-  double p = 0.075 / 12; // 7.5% annual interest rate 
+  double p = interestDecimal / 12; // 7.5% annual interest rate 
   double T = years * 12; 
   double S = D * ((pow(1 + p, T) - 1) / p);
 
@@ -25,7 +53,7 @@ int main()
   cout << D << " deposited per month will grow to $"; 
 
   // formatting output (see 4.2) 
-  cout.setf(ious::fixed|ios::showpoint);
+  cout.setf(ios::fixed|ios::showpoint);
   cout << setprecision(2); 
   cout << S << "." << endl; 
 
