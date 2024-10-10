1fe6b75482f35ec8d4e6d9f299553de8d653c6ec
Omar Shishani <omarshishanischool@gmail.com>
2024-10-09 17:15:34 -0700

    Place code inside 'while'

--- week-7/7.3/changeDue4.cpp
+++ week-7/7.3/changeDue4.cpp
@@ -15,71 +15,78 @@ int main()
   int cashPayment;
   int amountTendered;
 
-  cout << "Please enter the required cash payment amount: ";
-  cin >> cashPayment;
+  while (true){
+    cout << "Please enter the required cash payment amount [enter 0 to exit]: ";
+    cin >> cashPayment;
 
-  cout << endl; 
-  cout << "Please enter the amount tendered: ";
-  cin >> amountTendered;
+    if (cashPayment == 0) {
+      break;
+    }
 
-  int changeDue = amountTendered - cashPayment;
+    cout << endl; 
+    cout << "Please enter the amount tendered: ";
+    cin >> amountTendered;
 
-  cout << "Cash payment amount: " << cashPayment << endl;
-  cout << "Tendered amount: " << amountTendered << endl;
-  cout << "Change due: " << changeDue << endl;
-  
-  int hundredThousands = changeDue / 100000;
-  changeDue = changeDue % 100000;
-  int tenThousands = changeDue / 10000;
-  changeDue = changeDue % 10000;
-  int fiveThousands = changeDue / 5000; 
-  changeDue = changeDue % 5000;
-  int oneThousands = changeDue / 1000; 
-  changeDue = changeDue % 1000; //Remainder after paying out all 5000 bills
-  int fiveHundreds = changeDue / 500; 
-  changeDue = changeDue % 500; 
-  int oneHundreds = changeDue / 100; 
-  changeDue = changeDue % 100; 
-  int fifties = changeDue / 50; 
-  changeDue = changeDue % 50; 
-  int twenties = changeDue / 20; 
-  changeDue = changeDue % 20; 
-  int tens = changeDue / 10; 
-  changeDue = changeDue % 10;
-  // cout << " changeDue after tens: " << changeDue << endl; 
-  int fives = changeDue / 5; 
-  changeDue = changeDue % 5;  
-  // cout << " changeDue after fives: " << changeDue << endl;
-  int twos = changeDue / 2; 
-  changeDue = changeDue % 2;  
-  // cout << " changeDue after twos: " << changeDue << endl;
-  int ones = changeDue / 1; 
-  changeDue = changeDue % 1; 
+    int changeDue = amountTendered - cashPayment;
 
-  if (hundredThousands > 0)
-    cout << " this many hundred thousand dollar bills: " << hundredThousands << endl;
-  if (tenThousands > 0)
-    cout << " this many ten thousand dollar bills: " << tenThousands << endl;
-  if (fiveThousands > 0)
-    cout << " this many five thousand dollar bills: " << fiveThousands << endl;
-  if (oneThousands > 0)
-    cout << " this many one thousand dollar bills: " << oneThousands << endl;
-  if (fiveHundreds > 0)
-    cout << " this many five hundred dollar bills: " << fiveHundreds << endl;
-  if (oneHundreds > 0)
-    cout << " this many one hundred dollar bills: " << oneHundreds << endl;
-  if (fifties > 0)
-    cout << " this many fifty dollar bills: " << fifties << endl;
-  if (twenties > 0)
-    cout << " this many twenty dollar bills: " << twenties << endl;
-  if (tens > 0)
-    cout << " this many ten dollar bills: " << tens << endl;
-  if (fives > 0)
-    cout << " this many five dollar bills: " << fives << endl;
-  if (twos > 0)
-    cout << " this many two dollar bills: " << twos << endl;
-  if (ones > 0)
-    cout << " this many one dollar bills: " << ones << endl;
+    cout << "Cash payment amount: " << cashPayment << endl;
+    cout << "Tendered amount: " << amountTendered << endl;
+    cout << "Change due: " << changeDue << endl;
+    
+    int hundredThousands = changeDue / 100000;
+    changeDue = changeDue % 100000;
+    int tenThousands = changeDue / 10000;
+    changeDue = changeDue % 10000;
+    int fiveThousands = changeDue / 5000; 
+    changeDue = changeDue % 5000;
+    int oneThousands = changeDue / 1000; 
+    changeDue = changeDue % 1000; //Remainder after paying out all 5000 bills
+    int fiveHundreds = changeDue / 500; 
+    changeDue = changeDue % 500; 
+    int oneHundreds = changeDue / 100; 
+    changeDue = changeDue % 100; 
+    int fifties = changeDue / 50; 
+    changeDue = changeDue % 50; 
+    int twenties = changeDue / 20; 
+    changeDue = changeDue % 20; 
+    int tens = changeDue / 10; 
+    changeDue = changeDue % 10;
+    // cout << " changeDue after tens: " << changeDue << endl; 
+    int fives = changeDue / 5; 
+    changeDue = changeDue % 5;  
+    // cout << " changeDue after fives: " << changeDue << endl;
+    int twos = changeDue / 2; 
+    changeDue = changeDue % 2;  
+    // cout << " changeDue after twos: " << changeDue << endl;
+    int ones = changeDue / 1; 
+    changeDue = changeDue % 1; 
+
+    if (hundredThousands > 0)
+      cout << " this many hundred thousand dollar bills: " << hundredThousands << endl;
+    if (tenThousands > 0)
+      cout << " this many ten thousand dollar bills: " << tenThousands << endl;
+    if (fiveThousands > 0)
+      cout << " this many five thousand dollar bills: " << fiveThousands << endl;
+    if (oneThousands > 0)
+      cout << " this many one thousand dollar bills: " << oneThousands << endl;
+    if (fiveHundreds > 0)
+      cout << " this many five hundred dollar bills: " << fiveHundreds << endl;
+    if (oneHundreds > 0)
+      cout << " this many one hundred dollar bills: " << oneHundreds << endl;
+    if (fifties > 0)
+      cout << " this many fifty dollar bills: " << fifties << endl;
+    if (twenties > 0)
+      cout << " this many twenty dollar bills: " << twenties << endl;
+    if (tens > 0)
+      cout << " this many ten dollar bills: " << tens << endl;
+    if (fives > 0)
+      cout << " this many five dollar bills: " << fives << endl;
+    if (twos > 0)
+      cout << " this many two dollar bills: " << twos << endl;
+    if (ones > 0)
+      cout << " this many one dollar bills: " << ones << endl;
+  }
+
 
   // identifying output statements
   cout << endl; //Extra line
