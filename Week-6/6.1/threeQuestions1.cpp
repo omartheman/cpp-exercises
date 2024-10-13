8ac1a10cbb4db10aa221dcf807a800efbf5bd305
Omar Shishani <omarshishanischool@gmail.com>
2024-10-13 11:16:21 -0700

    Fix 6.1

--- Week-6/6.1/threeQuestions1.cpp
+++ Week-6/6.1/threeQuestions1.cpp
@@ -11,6 +11,14 @@ using namespace std;
 
 int main()
 {
+  // identifying output statements
+  cout << endl; // Extra line 
+  cout << "Programmer: Omar Shishani" << endl;
+  cout << "Description: This program asks the user to" << endl;
+  cout << " solve three addition/subtraction problems." << endl; 
+  cout << " The input is from the console keyboard," << endl;
+  cout << " and output is to the console screen." << endl;
+
   int answer1; 
   cout << "4 + 9 = ";
   cin >> answer1; 
@@ -19,12 +27,25 @@ int main()
     cout << "Your answer is correct!" << endl;
   else 
     cout << "Your answer is incorrect." << endl;
+  ;
 
-  // identifying output statements
-  cout << endl; // Extra line 
-  cout << "Programmer: Omar Shishani" << endl;
-  cout << "Description: This program asks the user to" << endl;
-  cout << " solve three addition/subtraction problems." << endl; 
-  cout << " The input is from the console keyboard," << endl;
-  cout << " and output is to the console screen." << endl;
+  int answer2; 
+  cout << "1 - 10 = ";
+  cin >> answer2; 
+
+  if (answer2 == -9)
+    cout << "Your answer is correct!" << endl;
+  else 
+    cout << "Your answer is incorrect." << endl;
+  ;
+
+  int answer3; 
+  cout << "100 + 200 = ";
+  cin >> answer3; 
+
+  if (answer3 == 300)
+    cout << "Your answer is correct!" << endl;
+  else 
+    cout << "Your answer is incorrect." << endl;
+  ;
 }
