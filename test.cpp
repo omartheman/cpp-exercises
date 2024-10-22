d172baf2dcdb51ec71c8802208663d7af6d474b8
Omar Shishani <omarshishanischool@gmail.com>
2024-10-21 17:57:30 -0700

    Add testing file

---
+++ test.cpp
@@ -0,0 +1,38 @@
+// Lab 4.1
+// Programmer: Omar Shishani
+// Editor(s) used: VSCode
+// Compiler(s) used: Apple clang version 14.0.0
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+int main()
+{
+  //Write an if-statement (or a series of if-statements) that assigns to the variable smallest the smallest vaue contained in variables i, j and k.  Assume that the three values are distinct.
+
+// Write a switch statement that does the following:
+// if the value of grade is
+
+// 'A', add 4 to sum
+// 'B', add 3 to sum
+// 'C', add 2 to sum
+// 'D', add 1 to sum
+// 'F', print "Student is on probation"
+
+// And print an error message if grade does not equal one of the five possible grades.
+// *************************************************
+// Continue with the code below:
+
+// switch (grade)
+// {
+  int val = 0; 
+
+  for (int i = 1; i < 11; i++){
+    val = val + i; 
+  }
+
+
+}
