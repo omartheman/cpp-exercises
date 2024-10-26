7fa55b8280cf425f9ba067285cd58f930d85697f
Omar Shishani <omarshishanischool@gmail.com>
2024-10-26 11:59:02 -0700

    Copy over mortgage calculator

--- ch8/8.3/secureMortgageCalculator1.cpp
+++ ch8/8.3/secureMortgageCalculator1.cpp
@@ -1,4 +1,4 @@
-// Lab 4.1
+// Lab 8.3
 // Programmer: Omar Shishani
 // Editor(s) used: VSCode
 // Compiler(s) used: Apple clang version 14.0.0
@@ -6,16 +6,68 @@
 
 #include <iostream>
 #include <string>
+#include <cmath>
+#include <iomanip>
 
 using namespace std; 
 
 int main()
 {
+  int p;
+  double r; //monthly decmial interest rate 
+  double annualInterestRate; 
+  int n; //Number of monthly payments
+  int yearsForPayback;
+
+  cout << "Please enter mortgage amount in dollars: ";
+  cin >> p; //Mortgage amount
+
+  double annualInterestRatePercentage; 
+  cout << endl; 
+  cout << "Please enter the interest rate." << endl; 
+  cout << "You can use as many decimal places as you like:" << endl;
+  cin >> annualInterestRatePercentage; 
+
+  cout << endl; //Extra line 
+  cout << "Rate parsed from input: " << annualInterestRatePercentage; 
+  cout << endl;
+
+  annualInterestRate = annualInterestRatePercentage / 100;
+  yearsForPayback = 30;
+
+  n = yearsForPayback * 12; //Months for payback
+  r =  annualInterestRate / 12; //Monthly decimal interest rate
+
+  double interestRateFactor;
+  interestRateFactor = pow((1 + r), n);
+
+  double monthlyPayment; 
+  monthlyPayment = p * interestRateFactor * r / (interestRateFactor - 1);
+
+  double annualInterestRateDisplay; 
+  annualInterestRateDisplay = annualInterestRate * 100; 
+
+  cout << "Amount borrowed (user input) = $" << p << endl; 
+  cout << "Annual interest rate (user input) = "; 
+  
+  cout.setf(ios::fixed|ios::showpoint);
+  cout << setprecision(3); 
+  cout << annualInterestRateDisplay << "%" << endl;
+
+  cout << "Payback period (programmer input) = ";
+  cout << setprecision(0); 
+  cout << yearsForPayback << " Years" << endl; 
+
+  cout << "Monthly payment (calculated output) = $";
+  //Format output 
+  cout.setf(ios::fixed|ios::showpoint);
+  cout << setprecision(2); 
+  cout << monthlyPayment << endl;
+
+  cout << endl; // Extra blank line
   // identifying output statements
-  cout << endl; // Extra line 
   cout << "Programmer: Omar Shishani" << endl;
-  cout << "Description: This program converts pounds" << endl;
-  cout << " to kilograms." << endl; 
-  cout << " The input is from the console keyboard," << endl;
-  cout << " and output is to the console screen." << endl << endl;
-} //main() 
+  cout << "Description: This program calculates the monthly" << endl;
+  cout << "payment on a loan. The inputs come from the programmer and the user," << endl;
+  cout << "and output is to the console screen." << endl;
+}
