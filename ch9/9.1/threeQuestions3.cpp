d7320c72c9e0f00d552508027f9b7eb31b9018dc
Omar Shishani <omarshishanischool@gmail.com>
2024-10-31 15:28:27 -0700

    Update threeQuestions3.cpp

--- ch9/9.1/threeQuestions3.cpp
+++ ch9/9.1/threeQuestions3.cpp
@@ -9,13 +9,32 @@
 
 using namespace std; 
 
+int askQuestion(string question, string correctAnswer){
+
+  cout << question << ": ";
+
+  string userAnswer; 
+  getline(cin, userAnswer); cout << endl; 
+
+  if (userAnswer == correctAnswer){
+    cout << "Correct!" << endl; 
+  }
+  else {
+    cout << "Incorrect." << endl; 
+  }
+}
+
 int main()
 {
   // identifying output statements
   cout << endl; // Extra line 
   cout << "Programmer: Omar Shishani" << endl;
-  cout << "Description: This program converts pounds" << endl;
-  cout << " to kilograms." << endl; 
+  cout << "Description: This program asks the user three different questions," << endl;
+  cout << " and checks the user's answers." << endl; 
   cout << " The input is from the console keyboard," << endl;
-  cout << " and output is to the console screen." << endl << endl;
-} //main() 
+  cout << " and output is to the console screen." << endl;
+
+  askQuestion("What is the last name of the current US President?", "Biden");
+  askQuestion("How many states are in the United States?", "50");
+  askQuestion("What is the capital city of California?", "Sacramento");
+}
