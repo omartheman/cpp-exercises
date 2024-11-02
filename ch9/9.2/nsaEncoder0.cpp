a21704485f24a05c45805a03ee56dccf3084a6a1
Omar Shishani <omarshishanischool@gmail.com>
2024-11-01 20:40:35 -0700

    Finish nsaEncoder0

--- ch9/9.2/nsaEncoder0.cpp
+++ ch9/9.2/nsaEncoder0.cpp
@@ -14,8 +14,22 @@ int main()
   // identifying output statements
   cout << endl; // Extra line 
   cout << "Programmer: Omar Shishani" << endl;
-  cout << "Description: This program converts pounds" << endl;
-  cout << " to kilograms." << endl; 
+  cout << "Description: This program takes a string from" << endl;
+  cout << " a user entry, and encodes it." << endl; 
   cout << " The input is from the console keyboard," << endl;
   cout << " and output is to the console screen." << endl << endl;
+
+  cout << "Please enter a line of text: "; 
+
+  string userEntry; 
+  getline(cin, userEntry); 
+
+  for (int i = 0; i < userEntry.length(); i++){
+    userEntry[i]++; 
+    // cout << endl << "New letter: " << userEntry[i] << endl; //For testing
+  }
+
+  cout << "New secret string: " << userEntry << endl; 
+
+
 } //main() 
