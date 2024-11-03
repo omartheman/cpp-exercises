17449886b0a55e83dfee93bac5a16a28415576a1
Omar Shishani <omarshishanischool@gmail.com>
2024-11-03 10:17:35 -0800

    Create myBillions.cpp

---
+++ ch9/9.5/myBillions.cpp
@@ -0,0 +1,41 @@
+// Lab 9.3
+// Programmer: Omar Shishani
+// Editor(s) used: VSCode
+// Compiler(s) used: Apple clang version 14.0.0
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+int main()
+{
+  // identifying output statements
+  cout << "Programmer: Omar Shishani" << endl;
+  cout << "Description: This program converts pounds" << endl;
+  cout << " to kilograms." << endl; 
+  cout << " The input is from the console keyboard," << endl;
+  cout << " and output is to the console screen." << endl << endl;
+
+  //Calculate the number of weeks it takes to be a billionaire. 
+  // A billion is 1,000,000,000.00 
+  // You start at 0.05 at the end of the first week, and double every week 
+
+  double total; 
+  double incrementCount; 
+
+  for (total = 0; total >= 1000; total += (0.05 * i)) {
+
+    //i needs to double each week
+    //First week i get 5 cents
+    //Second week I get 10 cents 
+    //Third week I get 20 cents 
+    //Fourth week I get 40 cents 
+    total += i;
+
+    cout << "Current number of weeks: " << i << endl; 
+    cout << 
+  }
+
+} //main() 
