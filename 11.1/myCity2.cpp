bc607a46736daa40ee998ce9c8a22c15a02c6a3c
Omar Shishani <omarshishanischool@gmail.com>
2024-11-07 17:17:24 -0800

    Create myCity2.cpp

---
+++ 11.1/myCity2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string> 
+
+using namespace std; 
+
+int main() 
+{
+  int highTempSep14; 
+  int highTempSep15;
+  int highTempSep16;
+  int highTempSep17;
+  int highTempSep18;
+
+  highTempSep14 = 91;
+  highTempSep15 = 83;
+  highTempSep16 = 72;
+  highTempSep17 = 73;
+  highTempSep18 = 78; 
+  cout << "Walnut Creek, CA forecast high temperatures:" << endl; 
+  cout << "Saturday, Sep 14, " << highTempSep14 << " degrees." << endl; 
+  cout << "Sunday, Sep 15, " << highTempSep15 << " degrees." << endl; 
+  cout << "Monday, Sep 16, " << highTempSep16 << " degrees." << endl; 
+  cout << "Tuesday, Sep 17, " << highTempSep17 << " degrees." << endl; 
+  cout << "Wednesday, Sep 18, " << highTempSep18 << " degrees." << endl;
+  cout << "Source: https://weather.com/" << endl; 
+}
+
