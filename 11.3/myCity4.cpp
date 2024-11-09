669e221134352d8f0b44bbc492ffa6420b2c9fc6
Omar Shishani <omarshishanischool@gmail.com>
2024-11-09 14:15:32 -0800

    Add 5 inputs

--- 11.3/myCity4.cpp
+++ 11.3/myCity4.cpp
@@ -12,25 +12,27 @@ int main()
   cout << " The input is from the programmer," << endl;
   cout << " and output is to the console screen." << endl << endl;
 
-  int highTempSep14; 
-  int highTempSep15;
-  int highTempSep16;
-  int highTempSep17;
-  int highTempSep18;
-
   const int SIZE = 5; 
 
   int temps[5];
 
-  cout << "Enter the high for Walnut Creek on Fri, Nov 8th: " << endl;
-  cin >> temps[0];
+  cout << "Enter the high for Walnut Creek on Fri, Nov 8th: ";
+  cin >> temps[0]; cout << endl; 
+  cout << "Enter the high for Walnut Creek on Sat, Nov 9th: ";
+  cin >> temps[1]; cout << endl; 
+  cout << "Enter the high for Walnut Creek on Sun, Nov 10th: ";
+  cin >> temps[2]; cout << endl; 
+  cout << "Enter the high for Walnut Creek on Mon, Nov 11th: ";
+  cin >> temps[3]; cout << endl; 
+  cout << "Enter the high for Walnut Creek on Tue, Nov 12th: ";
+  cin >> temps[4];  cout << endl; 
 
   cout << "Walnut Creek, CA forecast high temperatures:" << endl; 
   cout << "Fri, Nov 8th: " << temps[0] << " degrees." << endl; 
-  // cout << "Sunday, Sep 15, " << temps[1] << " degrees." << endl; 
-  // cout << "Monday, Sep 16, " << temps[2] << " degrees." << endl; 
-  // cout << "Tuesday, Sep 17, " << temps[3] << " degrees." << endl; 
-  // cout << "Wednesday, Sep 18, " << temps[4] << " degrees." << endl;
+  cout << "Sat, Nov 9th: " << temps[1] << " degrees." << endl; 
+  cout << "Sun, Nov 10th: " << temps[2] << " degrees." << endl; 
+  cout << "Mon, Nov 11th: " << temps[3] << " degrees." << endl; 
+  cout << "Tue, Nov 12th: " << temps[4] << " degrees." << endl; 
 
   cout << "Source: https://weather.com/" << endl; 
 
