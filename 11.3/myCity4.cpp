f0f8dae0159f612960fe10fd81c55e07eef3c36d
Omar Shishani <omarshishanischool@gmail.com>
2024-11-08 22:34:48 -0800

    Create myCity4.cpp

---
+++ 11.3/myCity4.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string> 
+
+using namespace std; 
+
+int main() 
+{
+  // identifying output statements
+  cout << "Programmer: Omar Shishani" << endl;
+  cout << "Description: This program uses an array to print the" << endl;
+  cout << " high temperatures for 5 different days." << endl; 
+  cout << " The input is from the programmer," << endl;
+  cout << " and output is to the console screen." << endl << endl;
+
+  int highTempSep14; 
+  int highTempSep15;
+  int highTempSep16;
+  int highTempSep17;
+  int highTempSep18;
+
+  const int SIZE = 5; 
+
+  int temps[] = {
+    91, 
+    83,
+    72, 
+    73, 
+    78
+  };
+
+  cout << "Walnut Creek, CA forecast high temperatures:" << endl; 
+  cout << "Saturday, Sep 14, " << temps[0] << " degrees." << endl; 
+  cout << "Sunday, Sep 15, " << temps[1] << " degrees." << endl; 
+  cout << "Monday, Sep 16, " << temps[2] << " degrees." << endl; 
+  cout << "Tuesday, Sep 17, " << temps[3] << " degrees." << endl; 
+  cout << "Wednesday, Sep 18, " << temps[4] << " degrees." << endl;
+  cout << "Source: https://weather.com/" << endl; 
+
+  int max = temps[0];
+  int min = temps[0];
+
+  for (int i = 1; i < SIZE; i++) {
+    if (max < temps[i]) max = temps[i];
+    if (min > temps[i]) min = temps[i]; 
+  }
+
+  cout << "The high for the week is " << max << " degrees." << endl;
+  cout << "The low for the week is " << min << " degrees." << endl; 
+
+}
