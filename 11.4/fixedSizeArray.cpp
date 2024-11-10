4992fb8b231f87f58ad2426c164f4709e4a19889
Omar Shishani <omarshishanischool@gmail.com>
2024-11-09 17:49:46 -0800

    Copy avgFile2.cpp from book

--- 11.4/fixedSizeArray.cpp
+++ 11.4/fixedSizeArray.cpp
@@ -4,6 +4,7 @@
 // Compiler(s) used: Apple clang version 14.0.0
 //
 
+#include <fstream> 
 #include <iostream>
 #include <string>
 
@@ -17,4 +18,37 @@ int main()
   cout << " to kilograms." << endl; 
   cout << " The input is from the console keyboard," << endl;
   cout << " and output is to the console screen." << endl << endl;
+
+
+  //open 8scores.txt for input 
+
+  ifstream fin; 
+  fin.open("8Scores.txt"); 
+  if (!fin.good()) throw "I/O error"; 
+
+  //read and save the scored
+  int score[8];
+  int i; //loop counter 
+  for (i = 0; i < 8; i++){
+    fin >> score[i];
+    fin.ignore(1000, 10); 
+  } // for 
+  fin.close(); 
+
+  //calculate and output the average 
+  int scoreTotal = 0; 
+  for (i = 0; i < 8; i++) {
+    scoreTotal += score[i]; 
+  } // for 
+  double average = scoreTotal / 8.0;
+  cout << "The average of 8 numbers is " << average << endl; 
+
+  // count # of scores > average 
+  int nGreater = 0; 
+  for (i = 0; i < 8; i++){
+    if (score[i] > average) nGreater++; 
+  } // for 
+
+  cout << nGreater << " scores are greater than the average." << endl; 
+
 } //main() 
