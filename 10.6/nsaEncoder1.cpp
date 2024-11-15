1477bd9dfd40c76cb75ff016b772acfe3f34d74d
Omar Shishani <omarshishanischool@gmail.com>
2024-11-14 21:16:51 -0800

    Create nsaEncoder1.cpp

---
+++ 10.6/nsaEncoder1.cpp
@@ -0,0 +1,35 @@
+// Lab 10.6
+// Programmer: Omar Shishani
+// Editor(s) used: VSCode
+// Compiler(s) used: Apple clang version 14.0.0
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+int main()
+{
+  // identifying output statements
+  cout << endl; // Extra line 
+  cout << "Programmer: Omar Shishani" << endl;
+  cout << "Description: This program takes a string from" << endl;
+  cout << " a user entry, and encodes it." << endl; 
+  cout << " The input is from a file," << endl;
+  cout << " and output is to the console screen." << endl << endl;
+
+  cout << "Please enter a line of text: "; 
+
+  string userEntry; 
+  getline(cin, userEntry); 
+
+  for (int i = 0; i < userEntry.length(); i++){
+    userEntry[i]++; 
+    // cout << endl << "New letter: " << userEntry[i] << endl; //For testing
+  }
+
+  cout << "New secret string: " << userEntry << endl; 
+
+
+} //main() 
