cceb265fe85b94189757aff4cc4af29f337d3dfe
Omar Shishani <omarshishanischool@gmail.com>
2024-11-26 20:22:27 -0800

    Create email12.cpp

---
+++ final/email12.cpp
@@ -0,0 +1,51 @@
+// Lab: Final
+// Programmer: Omar Shishani
+// Editor(s) used: VSCode
+// Compiler(s) used: Apple clang version 14.0.0
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+int main()
+{
+  // identifying output statements
+  cout << "Programmer: Omar Shishani" << endl;
+  cout << "Description: This program ..." << endl;
+  cout << " ..." << endl; 
+  cout << " The input is from the console keyboard," << endl;
+  cout << " and output is to the console screen." << endl << endl;
+
+  // Get input file name 
+  string fileName; 
+  string defaultFileName = "fileContainingEmails.txt"; 
+
+  cout << "Enter input file name [default: " << defaultFileName << "]: "; 
+  getline(cin, fileName); 
+  cout << endl; 
+
+  if (fileName.length() == 0){
+    fileName = defaultFileName; 
+  }
+
+  // Get output file name 
+  string defaultOutputFileName = "copyPasteMyEmails.txt"; 
+  string outputFileName; 
+
+  cout << "Enter output file name [default:" << defaultOutputFileName << "]:"; 
+  getline(cin, outputFileName); 
+  cout << endl; 
+
+  cout << "Length: "; 
+  cout << outputFileName.length(); 
+
+  if (outputFileName.length() == 0){
+    outputFileName = defaultOutputFileName; 
+  }
+
+  cout << endl << "Input file name: " << fileName << endl; 
+  cout << "Output file name: " << outputFileName << endl; 
+  
+} //main() 
