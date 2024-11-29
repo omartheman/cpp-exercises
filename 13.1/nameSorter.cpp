7b170b4b12e61b7666be226617cac61447bbbb49
Omar Shishani <omarshishanischool@gmail.com>
2024-11-28 18:36:48 -0800

    Add code to read file and input file name

--- 13.1/nameSorter.cpp
+++ 13.1/nameSorter.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include <string>
+#include <fstream> 
 
 using namespace std; 
 
@@ -18,5 +19,33 @@ int main()
   cout << " The input is from the console keyboard," << endl;
   cout << " and output is to the console screen." << endl << endl;
 
+  //Read file names 
+  ifstream fin;   
+
+  //Have user input file name 
+  cout << "Please enter file name for a file containing names to sort: "; 
+  string fileName; 
+  cin >> fileName; 
+
+  //Open file 
+  fin.open(fileName.c_str());
+  if (!fin.good()) throw "I/O error"; 
+
+  const int MAX_NAMES = 5; 
+  int nNames = 0;
+  string name[MAX_NAMES]; 
+
+  string aName; // Value to add 
+  //Set aName's value
+  fin >> aName; 
+  if (nNames < MAX_NAMES) {
+    name[nNames++] = aName; 
+  };
+
+  cout << "Printing names..." << endl; 
   
+  for (int i = 1; i < nNames; i++){
+    cout << "Name: " << name[i];
+  }
+
 } //main() 
